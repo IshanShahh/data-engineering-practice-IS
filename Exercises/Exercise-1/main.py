9e535f275c298a4a225d70c9be4a461703d067df
Ishan Shah <shah.ishan111@gmail.com>
2025-05-01 09:59:16 -0400

    worked on exercise 1 and 2

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,6 @@
 import requests
+import os 
+import zipfile
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -10,10 +12,37 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+path_to_folder = '/Users/Ishan/data-engineering-practice-IS/Exercises/Exercise-1/downloads'
+os.makedirs(path_to_folder, exist_ok=True)
+
+files = []
+print(files)
 
 def main():
     # your code here
-    pass
+    try:
+        for url in download_uris:
+            param = url.split('/')[-1]
+            x = url.replace(param,'')
+            response = requests.get(url)
+            if response.status_code == 200:
+                param = url.split('/')[-1]
+                x = url.replace(param,'')
+                with zipfile.ZipFile(response) as zip_ref:
+                    for file_info in zip_ref.infolist():
+                        extracted_filepath = os.path.join(path_to_folder,param)
+                # param = url.split('/')[-1]
+                # x = url.replace(param,'')
+    except requests.exceptions.HTTPError as errh:
+        print("error")
+    
+    # for 
+        # response = requests.get(url)
+        # print(response)
+    # try:
+    #     response = requests.get("https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip")
+    #     print(response)
+    # except 
 
 
 if __name__ == "__main__":
--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,11 +1,42 @@
 import requests
 import pandas
+from bs4 import BeautifulSoup
+import os 
 
+file = '2024-01-19 10:27'
+path_to_folder = '/Users/Ishan/data-engineering-practice-IS/Exercises/Exercise-2/downloads'
+os.makedirs(path_to_folder, exist_ok=True)
 
 def main():
     # your code here
-    pass
+    response = requests.get("https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/")
+    soup_data = BeautifulSoup(response.text,'html.parser')
+    table_row = soup_data.find_all('tr')
+    # print(table_row)
+    
+    for data in table_row:
+        x = data.find_all('td')
+        if len(x) > 1:
+            dates = x[1].text.strip()
+            if dates == '2024-01-19 10:27':
+                file_link = x[0].find('a')
+                file_name = file_link['href']
+                file_api = requests.get(response.url + file_name)
+                if file_api.status_code == 200:
+                    file_path = os.path.join(path_to_folder,file_name)
+                    with open(file_path, 'wb') as f:
+                        f.write(file_api.content)
+                    print(f"Saved {file_name} to downloads")
+                else:
+                    print('Did not work')
 
+df = pandas.read_csv("/Users/Ishan/data-engineering-practice-IS/Exercises/Exercise-2/downloads/01023199999.csv")
+print(max(df['HourlyDryBulbTemperature']))
+
+        # z = x.find_all('<td')
+        # print
+        # date_column = x[1]
+        # print(date_column)
 
 if __name__ == "__main__":
     main()
